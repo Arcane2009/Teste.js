c03c5810b31896c7d6cb769c5b01eaed5b037429
Arcane3308 <gustavo.alves.santos26@escola.pr.gov.br>
2025-08-20 09:52:09 -0300

    Add files via upload

---
+++ teste.js
@@ -0,0 +1,11 @@
+console.log("Olá Mundo\n");
+console.log("Ah não sei professor\n");
+
+console.log("*Variáveis*\n");
+var genio = "Vinícius";
+console.log(genio);
+
+var partida = 2;
+var final = 15;
+var distancia = final - partida;
+console.log(distancia);
